eb108c6b66814007f89322cc61098ef903242e0b
NMakarevich <nikoladze529@mail.ru>
2022-11-04 15:25:55 +0300

    feat: add browser router

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import Main from './routes/main/main';
+import About from './routes/about/about';
+import NotFound from './routes/not-found/not-found';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App></App>}>
+          <Route index element={<Main></Main>}></Route>
+          <Route path="about" element={<About></About>}></Route>
+          <Route path="404" element={<NotFound></NotFound>}></Route>
+          <Route path="*" element={<Navigate to="404" />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
