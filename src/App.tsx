d1cc4300e508e2d999d4f0dfc0fe167664251263
NMakarevich <nikoladze529@mail.ru>
2022-11-04 15:26:49 +0300

    feat: update app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { NavLink, Outlet } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="header">
+        <div className="container">
+          <nav>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="about">About</NavLink>
+          </nav>
+        </div>
       </header>
-    </div>
+      <main className="main">
+        <div className="container">
+          <Outlet></Outlet>
+        </div>
+      </main>
+      <footer className="footer">
+        <div className="container"></div>
+      </footer>
+    </>
   );
 }
 
